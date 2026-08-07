631ecd9be77039cf86940db1708f016c65a7e590
rifatkhan97 <rifatkhan1212@gmail.com>
2026-08-07 20:40:14 +0600

    Install and configure Sentry for Next.js App Router manually

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   images: {
@@ -6,4 +7,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "fenalt",
+  project: "nextjs",
+  silent: !process.env.CI,
+});
