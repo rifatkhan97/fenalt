a0e28476e6376517443811d10da29516339eac58
rifatkhan97 <rifatkhan1212@gmail.com>
2026-08-08 00:22:41 +0600

    Increase logo size in Navbar and Footer by 30%

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,9 +38,9 @@ export default function Navbar() {
               <Image
                 src="/logo.png"
                 alt="Fenalt Logo"
-                width={170}
-                height={42}
-                className="h-8 lg:h-10 w-auto object-contain"
+                width={220}
+                height={55}
+                className="h-10 lg:h-12 w-auto object-contain"
                 priority
               />
             </Link>
